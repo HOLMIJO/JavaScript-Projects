2af0a10f2612c67c47fc5473a0331934779c213e
Joseph Holmin <holminj@icloud.com>
2021-10-28 18:48:04 -0500

    Updated JS

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -3,3 +3,25 @@ function Master_Mind() { //Defined function and gave name
     var result = str.fontcolor("blue"); //Used fontcolor method on string variable
     document.getElementById("Blue_Text").innerHTML = result; //Put value of result into "Blue_Text" ID of HTML element
 }
+
+function Book_Worm() { //Defines function and gave name
+    var sentence = "If everything else was gone"; //Defined variable with string
+    sentence += " I\'d be happiest with a book!"; //added operator to variable as string
+    document.getElementById("Concatenate").innerHTML = sentence; //Put value of result into "Concatenate" ID of HTML element 
+}
+
+
+
+function Math_Whiz(X, Y) { // code to be executed
+    return X * Y; //returns result of math problem
+}
+document.getElementById("math").innerHTML = Math_Whiz(5, 4); //puts result into "Math" ID of HTML
+
+function Sunshine() { //Defined function and gave name
+    document.getElementById("smile").innerHTML = "Good Morning, Sunshine!"; //Put value of "smile" ID into HTML element
+}
+
+function addition_Function() {
+    var addition = 10 + 2;
+    document.getElementById("Math").innerHTML = "10 + 2 =" + addition;
+}
