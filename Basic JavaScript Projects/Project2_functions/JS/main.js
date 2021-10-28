5a4199669dd59806325bc7a10d56a8e5b2416746
Joseph Holmin <holminj@icloud.com>
2021-10-27 21:38:17 -0500

    Initial commit

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function Master_Mind() { //Defined function and gave name
+    var str = "The Master Mind is feeling blue!"; //defined variable with string
+    var result = str.fontcolor("blue"); //Used fontcolor method on string variable
+    document.getElementById("Blue_Text").innerHTML = result; //Put value of result into "Blue_Text" ID of HTML element
+}
