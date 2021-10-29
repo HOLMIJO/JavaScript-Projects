8efe0651e34c2463973ba9a4ad550bba3c8e9174
Joseph Holmin <holminj@icloud.com>
2021-10-29 12:56:38 -0500

    Initial commit

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,7 @@
+function Some_Math(); //Defined function and gave name
+var Numbers = { //Defined variable
+    Ten = "10", //Defined variable with string
+    Five = 5 //Defined variable with number
+}
+document.write(typeof "10" + 5) //Put value in type of to display
+}
