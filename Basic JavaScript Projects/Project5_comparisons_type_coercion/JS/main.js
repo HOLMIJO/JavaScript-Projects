32f8cd3ca0eb57e38280ff1a6b13d7d36132d3f8
Joseph Holmin <holminj@icloud.com>
2021-10-29 13:41:18 -0500

    Updated HTML and JS

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,7 +1,3 @@
-function Some_Math(); //Defined function and gave name
-var Numbers = { //Defined variable
-    Ten = "10", //Defined variable with string
-    Five = 5 //Defined variable with number
-}
-document.write(typeof "10" + 5) //Put value in type of to display
-}
+document.write(typeof "10" + 5) //Put value of string and number in type of to display
+
+document.write("10" + 5);
