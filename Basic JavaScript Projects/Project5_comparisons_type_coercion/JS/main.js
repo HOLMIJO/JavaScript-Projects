6681b404dde5618db926302e8ad62a6dbc08354d
Joseph Holmin <holminj@icloud.com>
2021-10-29 16:00:17 -0500

    Updated JS

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,3 +1,35 @@
 document.write(typeof "10" + 5) //Put value of string and number in type of to display
 
 document.write("10" + 5);
+
+document.write(2E310);
+
+document.write(-3E310);
+
+document.write(10 > 2);
+
+document.write(10 < 2);
+
+console.log(2 + 2);
+
+console.log(4 > 5)
+
+document.write(10 == 10);
+
+document.write(3 == 11);
+
+X = 10;
+Y = 10;
+document.write(X === Y);
+
+X = 82;
+Y = "84";
+document.write(X === Y);
+
+X = 18;
+Y = "Eighteen";
+document.write(X === Y);
+
+A = "Joseph";
+B = "Joseph";
+document.write(A === B);
