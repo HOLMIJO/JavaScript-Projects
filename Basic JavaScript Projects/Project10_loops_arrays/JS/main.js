8e9f65dd95efafb697cb3ee519b7bf097beabe98
HOLMIJO <91238285+HOLMIJO@users.noreply.github.com>
2021-11-02 23:54:33 -0500

    Initial commit

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,8 @@
+function Call_Loop {
+    let text = "";
+    let i = 0;
+    while (i < 10) {
+    text += "<br>The number is " + i; i++;
+    }
+document.getElementById("Loop").innerHTML = text;
+}
