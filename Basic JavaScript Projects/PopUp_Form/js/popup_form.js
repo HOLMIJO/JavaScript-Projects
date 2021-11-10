82373d9b11842208d11178f4b4b41eaeb01d6abb
HOLMIJO <91238285+HOLMIJO@users.noreply.github.com>
2021-11-10 14:19:18 -0600

    Updated CSS and JS

--- Basic JavaScript Projects/PopUp_Form/js/popup_form.js
+++ Basic JavaScript Projects/PopUp_Form/js/popup_form.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 $("#slideshow > div:gt(0)").hide();
 
 setInterval(function() {
@@ -14,21 +14,4 @@ function openForm() {
 }
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
-=======
-$("#slideshow > div:gt(0)").hide();
-
-setInterval(function() {
-    $('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-}, 3000);
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-}
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
->>>>>>> a311a3173334e2c9462a496082966a4ae91482b3
 }
